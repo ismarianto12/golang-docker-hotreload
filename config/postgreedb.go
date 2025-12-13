80e8a2d8755a3a1262901defbf9a8cbcb886a2e7
rian <@rian>
2025-12-14 01:09:00 +0700

    adsa

--- config/postgreedb.go
+++ config/postgreedb.go
@@ -2,18 +2,24 @@ package config
 
 import (
 	"fmt"
-	"os"
+	"log"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func NewDB() (*gorm.DB, error) {
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USERNAME")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
+	// host := os.Getenv("DB_HOST")
+	// port := os.Getenv("DB_PORT")
+	// user := os.Getenv("DB_USERNAME")
+	// password := os.Getenv("DB_PASSWORD")
+	// dbname := os.Getenv("DB_NAME")
+
+	host := "postgres"
+	port := "5432"
+	user := "postgres"
+	password := "postgres"
+	dbname := "barang_db"
 
 	dsn := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=Asia/Jakarta",
@@ -22,6 +28,12 @@ func NewDB() (*gorm.DB, error) {
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
+		log.Printf("DB_HOST     : %s", host)
+		log.Printf("DB_PORT     : %s", port)
+		log.Printf("DB_USERNAME : %s", user)
+		log.Printf("DB_NAME     : %s", dbname)
+
+		fmt.Println("✅ Error to PostgreSQL with GORM")
 		return nil, err
 	}
 
--- repositories/BarangRepo.go
+++ repositories/BarangRepo.go
@@ -12,21 +12,73 @@ type BarangRepo struct {
 	db *gorm.DB
 }
 
-func NewBarangRepo(db *gorm.DB) *BarangRepo {
+func NewBarangRepo() *BarangRepo {
 	db, err := config.NewDB()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("❌ gagal create DB:", err)
 	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("❌ gagal ambil sql.DB:", err)
+	}
+
+	if err := sqlDB.Ping(); err != nil {
+		log.Fatal("❌ DB TIDAK TERKONEKSI:", err)
+	}
+
+	log.Println("✅ DB TERKONEKSI")
+
 	return &BarangRepo{db: db}
 }
 
 func (r *BarangRepo) GetAllData() ([]entities.Product, error) {
 	var data []entities.Product
-	log.Printf("info data", data)
+
+	db, err := config.NewDB()
+	if err != nil {
+		log.Fatal("❌ gagal create DB:", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("❌ gagal ambil sql.DB:", err)
+	}
+
+	if err := sqlDB.Ping(); err != nil {
+		log.Fatal("❌ DB TIDAK TERKONEKSI:", err)
+	}
+
+	log.Println("✅ DB TERKONEKSI")
+
+	getDb := r.db.Statement.Schema.DBNames
+	log.Printf("info data {}", getDb)
+
 	if err := r.db.Select("name").Scan(&data).Error; err != nil {
 		return data, err
 	}
 	return data, nil
+}
+
+func (r *BarangRepo) Delete(id int) error {
+	if err := r.db.Delete(&id).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *BarangRepo) FindById(id int) (*entities.Product, error) {
+	var data entities.Product
+	if err := r.db.First(&data, id).Error; err != nil {
+		return nil, err
+	}
+	return &data, nil
+}
+
+func (r *BarangRepo) SaveData(prod *entities.Product) error {
+	if err := r.db.Save(&prod).Error; err != nil {
+		return err
+	}
+	return nil
 }
 
 func (r *BarangRepo) CreateData(prod *entities.Product) error {
